0df8bc020a4bc77e7ac2f2821fc4b7c6127857c8
Adeyemo Aderonke Deborah <Adeyemooluwatoyin2018@gmail.com>
2022-09-09 15:30:42 -0700

    Task 6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -10,9 +10,11 @@ int main(void)
 	long li;
 	long long lili;
 	float f;
+
 	printf("Size of a char: %ld byte(s)\n", sizeof(c));
 	printf("Size of an int: %ld byte(s)\n", sizeof(i));
 	printf("Size of a long int: %ld byte(s)\n", sizeof(li));
 	printf("Size of a long long int: %ld byte(s)\n", sizeof(lili));
 	printf("Size of a float: %ld byte(s)\n", sizeof(f));
+	return (0);
 }
