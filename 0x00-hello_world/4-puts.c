efeb319df6914dd25aa6357b9a0e5432aa802424
Adeyemo Aderonke Deborah <Adeyemooluwatoyin2018@gmail.com>
2022-09-09 13:59:17 -0700

    Task 5

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,4 @@
-#include <stdio.h>
-/**
- * main - entry point
- * Return: always 0
+
  */
-int main(void)
-{
-	puts("\"Programming is like building a multilingual puzzle");
-	return (0);
-}
+int main
+
