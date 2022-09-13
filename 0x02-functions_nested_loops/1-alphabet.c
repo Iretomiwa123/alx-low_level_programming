e434ef8f5281b22cb3d1e2ab9707df3a48d9cba2
Adeyemo Aderonke Deborah <Adeyemooluwatoyin2018@gmail.com>
2022-09-13 10:23:00 -0700

    Task 1

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -3,8 +3,14 @@
  * main - entry point
  * Return: Always 0.
  */
-int main(void)
+void print_alphabet(void);
 {
-	print_alphabet();
+	int i;
+
+	for (i = 'a'; i <= 'z', i++)
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
 	return (0);
 }
