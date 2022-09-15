98d7a9b0afedabb7292a196ca814b2124dd5d180
Adeyemo Aderonke Deborah <Adeyemooluwatoyin2018@gmail.com>
2022-09-14 19:17:06 -0700

    11-print_to_98.c

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -6,13 +6,16 @@
  */
 void print_to_98(int n)
 {
-while (n != 98)
-{
-printf("%i,", n);
-if (n > 98)
-n--;
-else
-n++;
-}
-printf("98 \n");
+	if (n < 98)
+	{
+		for (n = n; n < 98; n++)
+			printf("%d, ", n);
+		printf("%d\n", 98);
+	}
+	else
+	{
+		for (n = n; n > 98; n--)
+			printf("%d, ", n);
+		printf("%d\n", 98);
+	}
 }
