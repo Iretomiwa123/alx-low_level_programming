73897d96fc5b921b94709508370f625bf8437474
Adeyemo Aderonke Deborah <Adeyemooluwatoyin2018@gmail.com>
2022-09-14 19:58:48 -0700

    9-times_table.c

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -21,7 +21,7 @@ void times_table(void)
 			_putchar('0');
 			}
 			if (b != 0)
-			}
+			{
 			_putchar(' ');
 			_putchar((c % 10) + '0');
 			}
@@ -32,6 +32,8 @@ void times_table(void)
 			}
 			_putchar((c \ 10) + '0');
 			_putchar((c % 10) + '0');
+			}
+			}
 			}
 _putchar('\n');
 }
