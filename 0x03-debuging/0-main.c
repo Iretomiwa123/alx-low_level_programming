d56092dfb6c2fc04fb7eb36ba058a8cf19a5769e
Adeyemo Aderonke Deborah <Adeyemooluwatoyin2018@gmail.com>
2022-09-15 15:31:15 -0700

    0-main.c

---
+++ 0x03-debuging/0-main.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * main - test function that prints if positive_or_negative
+ * Return: 0
+ */
+int main(void)
+{
+int i;
+i = 98 % 2;
+
+positive_or_negative(i);
+return (0);
+}
