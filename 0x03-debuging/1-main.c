76de2c20f5878f6abe64f17b2efcc682336f9ddb
Adeyemo Aderonke Deborah <Adeyemooluwatoyin2018@gmail.com>
2022-09-16 04:12:56 -0700

    1-main.c

---
+++ 0x03-debuging/1-main.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - causes an infinite loop
+ * Return: always 0
+ */
+int main(void)
+{
+	int i;
+		printf("Infinite loop incoming :(\n");
+	i = 0;
+	/**
+	*while (i < 10)
+	*{
+	*	_putchar(i);
+	*}
+	*/
+	prinf("Infinite loop avoided! \\0/\n");
+
+	return (0);
+}
