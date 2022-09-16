f87151bd434cb79e96b56145cec7f8d22b574da8
Adeyemo Aderonke Deborah <Adeyemooluwatoyin2018@gmail.com>
2022-09-16 15:04:09 -0700

    0-isupper.c

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,16 +1,14 @@
-#include <stdio.h>
 #include "main.h"
+#include <stdio.h>
 /**
  *  _isupper -  function that checks for uppercase character
  *  @c: parameter
- *  Return: return 0
+ *  Return: return 1 and 0
  */
 int _isupper(int c)
 {
-for (c = 'A'; c <= 'Z'; c++)
+if (c >= 'A' && c <= 'Z')
 return (1);
 else
-{
 return (0);
-}
 }
