a45ee989ef850d28143906bea6d10a09391fafd8
Adeyemo Aderonke Deborah <Adeyemooluwatoyin2018@gmail.com>
2022-09-19 13:16:31 -0700

    0-reset_to_98.c

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,9 +1,11 @@
 #include "main.h"
+
 /**
- * reset_to_98 - update value
+ * reset_to_98 - reset the value of the  given integer to 98
  * @n: parameter
- * Return: 0
+ * Return: always 0
  */
+
 void reset_to_98(int *n)
 {
 	*n = 98;
