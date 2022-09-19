ca261929b52bceff6b32f75788c5664e7c980fed
Adeyemo Aderonke Deborah <Adeyemooluwatoyin2018@gmail.com>
2022-09-19 14:17:41 -0700

    1-swap.c

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -2,7 +2,7 @@
 /**
  * swap_int - function that swaps the values of two integers
  * @a: first integer
- * @b; second integer
+ * @b: second integer
  * Return: void
  */
 void swap_int(int *a, int *b)
