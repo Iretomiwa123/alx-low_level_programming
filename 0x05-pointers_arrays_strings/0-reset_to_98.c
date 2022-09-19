31dcc9345396770b01594b4772e39d54e3b25f05
Adeyemo Aderonke Deborah <Adeyemooluwatoyin2018@gmail.com>
2022-09-19 04:03:23 -0700

    0-reset_to_98.c

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,5 +1,4 @@
 #include "main.h"
-#include <stdio.h>
 /**
  * main - check the code
  * int: parameter
@@ -10,7 +9,7 @@ int main(void)
 	int n;
 
 	n = 402;
-	printf("n = %d\n", n);
+	_putchar("n = %d\n", n);
 	reset_to_98(&n);
 	return (0);
 }
