1e60fdfc9d63728e4391d3268d628e40fb4c265b
Adeyemo Aderonke Deborah <Adeyemooluwatoyin2018@gmail.com>
2022-09-19 03:50:11 -0700

    0-reset_to_98.c

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * main - check the code
+ * int: parameter
+ * Return: 0
+ */
+int main(void)
+{
+	int n;
+
+	n = 402;
+	_putchar("n=%d\n", n);
+	reset_to_98(&n);
+	return (0);
+}
