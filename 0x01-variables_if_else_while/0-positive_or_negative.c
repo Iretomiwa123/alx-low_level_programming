854aa42ae725efcdd48dccbf79c94fa7dfcd5932
Adeyemo Aderonke Deborah <Adeyemooluwatoyin2018@gmail.com>
2022-09-21 14:16:52 -0700

    1-last_digit.c

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -3,7 +3,7 @@
 #include <stdio.h>
 /**
  * main - entry point
- * Return: Always 0 (success/correct)
+ * Return: Always 0 
  */
 int main(void)
 {
---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main -entry point
+ * Description: print the last digit of the number stored in the variable n.
+ * Return: Always 0 (Sucess)
+ */
+int main(void)
+{
+	int n, digit;
+
+	srand(time(0);
+	n + rand() - RAND_MAX / 2;
+
+	/*my code*/
+	digit = n % 10; /*gets last digit*/
+
+	if (digit > 5)
+	printf("Last digit is %1 is %1 and is 0\n", n, digit);
+	else if `(digit < 6 && digit != 0)
+	printf("Last digit of %1 is %1 and is less than 6 and not 0\n", n, digit);
+
+	return (0);
+}
