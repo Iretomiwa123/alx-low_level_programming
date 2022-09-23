ccac4a235888f871546a48086f99bccdfe0e0bf0
Adeyemo Aderonke Deborah <Adeyemooluwatoyin2018@gmail.com>
2022-09-23 08:17:34 -0700

    0-strcat.c

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _strcat -function that concatenates two strings
+ * @src: string
+ * @dest:  adds a terminating null byte
+ *Return: always 0
+ */
+char *_strcat(char *dest, char *src)
+{
+	int i, j;
+
+	i = 0;
+	while (dest[i] != '\0')
+	{
+		++i;
+	}
+	for (j = 0; src[j] != '\0'; j++, i++)
+	{
+		dest[i] = src[j];
+	}
+		dest[i] = '\0';
+		return (dest);
+}
