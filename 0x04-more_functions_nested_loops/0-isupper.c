43ac971a859f6bfe80da8a8c684a9642f6027961
Adeyemo Aderonke Deborah <Adeyemooluwatoyin2018@gmail.com>
2022-10-02 14:25:53 -0700

    0-isupper.c

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -2,13 +2,16 @@
 
 /**
  * _isupper -  function that checks for uppercase character.
- * @c: uppercase
+ *
+ * @c: value to be checked
+ *
  * Return: 1 if arg is uppercase,
+ *
  * 0 if arg it is not.
  */
 int _isupper(int c)
 {
-	if (c >= '65' && c <= '90')
+	if (c >= 65 && c <= 90)
 {
 	return (1);
 }
