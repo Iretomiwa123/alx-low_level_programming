dfa557757edcfed15d0a242c651fa19cdbf10ec8
Adeyemo Aderonke Deborah <Adeyemooluwatoyin2018@gmail.com>
2022-10-04 03:18:47 -0700

    3-alloc_grid.c

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -25,23 +25,24 @@ int **alloc_grid(int width, int height)
 
 	for (hgt_indx = 0; hgt_indx < height; hgt_indx++)
 	{
-		ptr_arr[hgt_indx] = malloc(sizeof(int) * width);
+	ptr_arr[hgt_indx] = malloc(sizeof(int) * width);
 
 		if (ptr_arr[hgt_indx] == NULL)
 		{
-			for (; hgt_indx >= 0; hgt_indx--)
-				free(ptr_arr[hgt_indx--]);
+		for (; hgt_indx >= 0; hgt_indx--)
+		free(ptr_arr[hgt_indx--]);
 
-			free(ptr_arr);
+		free(ptr_arr);
 
-			return (NULL);
+		return (NULL);
 		}
 	}
+
 	for (hgt_indx = 0; hgt_indx < height; hgt_indx++)
 	{
 	for (wid_indx = 0; wid_indx < width; wid_indx++)
 	ptr_arr[hgt_indx][wid_indx] = 0;
 	}
 
-	return (ptr_arr);
+	return (ptr_arr)
 }
