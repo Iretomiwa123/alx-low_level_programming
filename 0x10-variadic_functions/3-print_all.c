f88c11bf31bc5b25b77fad9f7dac5b4a5b7b8b2e
Adeyemo Aderonke Deborah <Adeyemooluwatoyin2018@gmail.com>
2022-11-01 17:21:35 -0700

    0x14-bit_manipulation

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -74,10 +74,8 @@ void print_all(const char * const format, ...)
 				sep = ", ";
 			}
 			j++;
-
 		}
 		i++;
-
 	}
 	printf("\n");
 	va_end(args);
--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-0x0F-function_pointers
+0x14-bit_manipulation
